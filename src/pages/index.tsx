1c06f906fad21453eea77ddf1d4ab95e9b8dcab6
Cobey Hollier <cobey.hollier@mac.com>
2023-02-05 11:05:08 +0100

    CH: Initial typescript setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { useState } from "react";
-import styles from "./index.module.css";
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
@@ -19,12 +18,15 @@ export default function Home() {
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
       setAnimalInput("");
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
@@ -38,8 +40,8 @@ export default function Home() {
         <link rel="icon" href="/dog.png" />
       </Head>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
+      <main>
+        <img src="/dog.png" />
         <h3>Name my pet</h3>
         <form onSubmit={onSubmit}>
           <input
@@ -51,7 +53,7 @@ export default function Home() {
           />
           <input type="submit" value="Generate names" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div>{result}</div>
       </main>
     </div>
   );
