7fbf058bba3593a4a46aca8b61b5945f4ff2bae0
Cobey Hollier <cobey.hollier@mac.com>
2023-02-10 11:06:26 +0100

    CH: Improving image search algorithm

--- components/ResultsDisplay.tsx
+++ components/ResultsDisplay.tsx
@@ -1,14 +1,16 @@
 import { Stack } from "@chakra-ui/react";
 import ResultCard from "./ResultCard";
 
-const ResultsDisplay = (props: { result: string }) => {
+const ResultsDisplay = (props: { result: string; location: string }) => {
   const DataRows = () => {
     const splitData = props.result.split(/\d+[.]/);
     let cards = [];
     for (let i = 0; i < splitData.length; i++) {
       const element = splitData[i];
       if (element.trim().length === 0) continue;
-      cards.push(<ResultCard text={element} key={i} />);
+      cards.push(
+        <ResultCard text={element} key={i} location={props.location} />
+      );
     }
     return <Stack>{...cards}</Stack>;
   };
